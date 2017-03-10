2781fd53316930827d0e0c4f41d6ca761ee0403d
Thomas Lin Pedersen <thomasp85@gmail.com>
2017-03-10 11:47:41 +0100

    Support last standard verbs

---
+++ R/distinct.R
@@ -0,0 +1,38 @@
+#' @importFrom dplyr distinct
+#' @importFrom rlang tidy_quotes tidy_quote as_symbol tidy_eval UQS
+#' @importFrom utils head
+#' @export
+distinct.tbl_graph <- function(.data, ..., .keep_all = FALSE) {
+  d_tmp <- as_tibble(.data)
+  if ('.tbl_graph_index' %in% names(d_tmp)) {
+    stop('The attribute name ".tbl_graph_index" is reserved', call. = FALSE)
+  }
+  orig_ind <- seq_len(nrow(d_tmp))
+  dot_list <- tidy_quotes(..., .named = TRUE)
+  if (length(dot_list) == 0) {
+    dot_list <- lapply(names(d_tmp), function(n) tidy_quote(!! as_symbol(n)))
+    names(dot_list) <- names(d_tmp)
+  }
+  d_tmp$.tbl_graph_index <- orig_ind
+
+  d_tmp <- tidy_eval(
+    tidy_quote(
+      distinct(d_tmp, UQS(dot_list), .keep_all = TRUE)
+    )
+  )
+
+  remove_ind <- orig_ind[-d_tmp$.tbl_graph_index]
+  graph <- switch(
+    active(.data),
+    nodes = delete_vertices(.data, remove_ind),
+    edges = delete_edges(.data, remove_ind)
+  ) %gr_attr% .data
+  if (!.keep_all) {
+    d_tmp <- d_tmp[, names(d_tmp) %in% names(dot_list), drop = FALSE]
+  } else {
+    d_tmp <- d_tmp[, names(d_tmp) != '.tbl_graph_index', drop = FALSE]
+  }
+  set_graph_data(graph, d_tmp)
+}
+#' @export
+dplyr::distinct
---
+++ R/sample_frac.R
@@ -0,0 +1,21 @@
+#' @export
+#' @importFrom dplyr sample_frac
+#' @importFrom igraph delete_vertices delete_edges
+sample_frac.tbl_graph <- function(tbl, size = 1, replace = FALSE, weight = NULL, .env = parent.frame()) {
+  d_tmp <- as_tibble(tbl)
+  if ('.tbl_graph_index' %in% names(d_tmp)) {
+    stop('The attribute name ".tbl_graph_index" is reserved', call. = FALSE)
+  }
+  orig_ind <- seq_len(nrow(d_tmp))
+  d_tmp$.tbl_graph_index <- orig_ind
+  d_tmp <- sample_frac(d_tmp, size = size, replace = replace, weight = weight, .env = .env)
+  remove_ind <- orig_ind[-d_tmp$.tbl_graph_index]
+  switch(
+    active(tbl),
+    nodes = delete_vertices(tbl, remove_ind),
+    edges = delete_edges(tbl, remove_ind)
+  ) %gr_attr% tbl
+}
+
+#' @export
+dplyr::sample_frac
