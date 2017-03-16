432c3af73e65015cb30625295bda7bc704285638
Thomas Lin Pedersen <thomasp85@gmail.com>
2017-03-16 15:26:22 +0100

    Consistent file structure

---
+++ R/igraph.R
@@ -0,0 +1,7 @@
+#' @describeIn as_tbl_graph Method for igraph object. Simply subclasses the object into a `tbl_graph`
+#' @export
+as_tbl_graph.igraph <- function(x, ...) {
+  class(x) <- c('tbl_graph', 'igraph')
+  attr(x, 'active') <- 'nodes'
+  x
+}
--- R/tbl_graph.R
+++ R/tbl_graph.R
@@ -75,13 +75,6 @@ print.tbl_graph <- function(x, ...) {
 is_tree <- function(x) {
   is_connected(x) && (gorder(x) - gsize(x) == 1)
 }
-#' @describeIn as_tbl_graph Method for igraph object. Simply subclasses the object into a `tbl_graph`
-#' @export
-as_tbl_graph.igraph <- function(x, ...) {
-  class(x) <- c('tbl_graph', 'igraph')
-  attr(x, 'active') <- 'nodes'
-  x
-}
 #' @export
 as_tbl_graph.tbl_graph <- function(x, ...) {
   x
