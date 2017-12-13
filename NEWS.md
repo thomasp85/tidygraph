70b26adb186ac7b9be9a84fc7d0c7267d9ecfe7a
Thomas Lin Pedersen <thomasp85@gmail.com>
2017-12-13 13:40:33 +0100

    Add set table to as_tbl_graph.data.frame

--- NEWS.md
+++ NEWS.md
@@ -5,6 +5,8 @@
 * Change license to MIT
 * Add `convert` verb to perform both `morph` and `crystallise` in one go, 
   returning a single `tbl_graph`
-
-
-
+* When collapsing edges or nodes during `morph` the original data will be stored
+  in `.orig_data` instead of `.data` to avoid conflicts with `.data` argument in
+  many tidyverse verbs (**BREAKING**)
+* `as_tbl_graph.data.frame` now recognises set tables (each column gives eachs
+  rows membership to that set)
--- R/data_frame.R
+++ R/data_frame.R
@@ -1,16 +1,54 @@
-#' @describeIn tbl_graph Method for edge table
+#' @describeIn tbl_graph Method for edge table and set membership table
 #' @export
 #' @importFrom igraph graph_from_data_frame
 as_tbl_graph.data.frame <- function(x, directed = TRUE, ...) {
+  graph <- switch(
+    guess_df_type(x),
+    edge_df = as_graph_edge_df(x, directed),
+    set_df = as_graph_set_df(x)
+  )
+
+  as_tbl_graph(graph)
+}
+guess_df_type <- function(x) {
+  if (all(c('to', 'from') %in% names(x))) return('edge_df')
+  if (all(vapply(x, inherits, logical(1), 'logical'))) return('set_df')
+  if (all(vapply(x, function(col) all(unique(col) %in% c(0,1)), logical(1)))) return('set_df')
+  'edge_df'
+}
+as_graph_edge_df <- function(x, directed) {
   from_ind <- which(names(x) == 'from')
   if (length(from_ind) == 0) from_ind <- 1
   to_ind <- which(names(x) == 'to')
   if (length(to_ind) == 0) to_ind <- 2
   x <- x[, c(from_ind, to_ind, seq_along(x)[-c(from_ind, to_ind)]), drop = FALSE]
-  graph <- graph_from_data_frame(x, directed = directed)
-  as_tbl_graph(graph)
+  graph_from_data_frame(x, directed = directed)
 }
-
+as_graph_set_df <- function(x, simple = TRUE) {
+  if (simple) {
+    x <- as.matrix(x)
+    mode(x) <- 'integer'
+    adj_mat <- x %*% t(x)
+    if (!is.null(attr(x, 'row.names'))) {
+      colnames(adj_mat) <- rownames(adj_mat) <- row.names(x)
+    }
+    as_graph_adj_matrix(adj_mat, FALSE)
+  } else {
+    edges <- do.call(rbind, lapply(names(x), function(name) {
+      nodes <- which(as.logical(x[[name]]))
+      edges <- expand.grid(nodes, nodes)
+      names(edges) <- c('from', 'to')
+      edges$type <- name
+      edges[edges$from != edges$to, , drop = FALSE]
+    }))
+    if (!is.null(attr(x, 'row.names'))) {
+      nodes <- data.frame(name = row.names(x), stringsAsFactors = FALSE)
+    } else {
+      nodes <- as.data.frame(matrix(ncol = 0, nrow = nrow(x)))
+    }
+    as_graph_node_edge(list(nodes = nodes, edges = edges), FALSE)
+  }
+}
 #' @export
 as.data.frame.tbl_graph <- function(x, row.names = NULL, optional = FALSE, active = NULL, ...) {
   as.data.frame(as_tibble(x, active = active))
