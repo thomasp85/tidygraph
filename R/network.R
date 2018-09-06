a177decff091a8080edf56bfa0a44c0fc7fdc391
knapply <syknapptic@gmail.com>
2018-09-06 11:38:24 -0700

    add missing importFrom igraph add_edges; drop comments; clean up format

--- R/network.R
+++ R/network.R
@@ -17,12 +17,11 @@ as_tbl_graph.network <- function(x, ...) {
 #' as an igraph object.
 #'
 #' @importFrom dplyr bind_rows
-#' @importFrom igraph graph_attr<- edge_attr<- vertex_attr<-
+#' @importFrom igraph add_edges graph_attr<- edge_attr<- vertex_attr<-
 #' @noRd
 #'
 network_to_igraph <- function(graph) {
   metadata <- graph$gal
-  # graph-level "attributes" to drop as they're better described as metadata
   metadata_names <- c("n", "directed", "hyper", "loops", "multiple", "bipartite", "mnext")
   graph_attrs <- metadata[!names(metadata) %in% metadata_names]
   if (metadata$hyper) {
@@ -30,40 +29,13 @@ network_to_igraph <- function(graph) {
   }
   
   node_attrs <- bind_rows(lapply(graph$val, `[`))
-  # {igraph} doesn't track "missing"/"na" vertices.
   node_attrs <- node_attrs[colnames(node_attrs) != "na"]
-  # {network} assumes vertex names are assigned to an attribute called "vertex.names"
-  # {igraph} assumes they are assigned to an attribute called "name"
   names(node_attrs)[names(node_attrs) == "vertex.names"] <- "name"
-  # both packages have instances where they expect vertex names to be their first attribute
   node_attrs <- node_attrs[, c("name", names(node_attrs)[names(node_attrs) != "name"])]
   
-  # "bipartite" networks are the weak spot:
-  #
-  # Both packages are problematic in conflating bipartite and two-mode networks, but do
-  # so in different ways. They are also both insufficiently strict about handling graphs 
-  # that are inherently not bipartite (e.g. they allow bipartite networks to have loops).
-  #
-  # {network} tracks the number of vertices belonging to the "actors" mode as a graph-level 
-  # attribute: `<network-object>$gal$bipartite`.
-  # 
-  # The count of vertices of the "non-actors" mode (e.g events, locations, etc.) are then 
-  # `<network-object>$gal$n - <network-object>$gal$bipartite`.
-  # 
-  # If `<network-object>$gal$bipartite` is numeric, it's considered "bipartite".
-  # If it's `FALSE` OR `NULL`, it's not. 
-  #
-  # This is in conflict with {igraph}, which explicitly uses a `logical` vertex attribute
-  # to track vertex modes, conventionally named "type".
-  # 
-  # The solution I've implemented in my own package is to assign "actors" (vertices with 
-  # IDs < `<network-object>$gal$bipartite`) as `TRUE`.
-  # This simplifies things when converting back to network-class objects as the "type"
-  # attribute can then be interpreted as "is_actor". This facilitates consistently accurate
-  # round-trip conversions.
   if (is.numeric(metadata$bipartite)) {
     if ("type" %in% names(node_attrs)) {
-      if(is.logical(node_attrs$type)) {
+      if (is.logical(node_attrs$type)) {
         warning('This network object is bipartite and uses a `logical` vertex attribute
                  named "type". igraph will assume that "type" is intended to label vertex 
                  modes in the resulting graph. Rename this vertex attribute if this is not 
@@ -80,22 +52,18 @@ network_to_igraph <- function(graph) {
   }
   
   edge_attrs <- bind_rows(lapply(graph$mel, `[[`, "atl"))
-  # {igraph} doesn't track "missing"/"na" edges.
   edge_attrs <- edge_attrs[colnames(edge_attrs) != "na"]
   
-  outl <- vapply(graph$mel, `[[`, numeric(1), "outl") # may be integer or double
+  outl <- vapply(graph$mel, `[[`, numeric(1), "outl")
   inl <- vapply(graph$mel, `[[`, numeric(1), "inl")
   if (metadata$directed) {
     el <- cbind(outl, inl)
   } else {
-    # {network} reverses egos and alters for undirected edge lists. Controling this allows
-    # validation of round-trip conversions.
     el <- cbind(inl, outl)
   }
-  # vectorizing edges allows them to be added to an empty graph
   el_vec <- as.vector(t(el))
-  # starting with an empty graph allows for isolates
-  new_graph <- igraph::graph.empty(graph$gal$n, directed = graph$gal$directed)
+
+  new_graph <- igraph::graph.empty(graph$gal$n, directed = isTRUE(graph$gal$directed))
   new_graph <- add_edges(new_graph, edges = el_vec)
   if (length(graph_attrs)) {
     graph_attr(new_graph) <- graph_attrs
