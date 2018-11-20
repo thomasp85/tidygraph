0aab03d8d15064de55cb40efd531b6edb8d2f545
Thomas Lin Pedersen <thomasp85@gmail.com>
2018-11-20 21:14:57 +0100

    Fix #78 Add fortify method for tbl_graph objects

---
+++ R/plot.R
@@ -0,0 +1,14 @@
+#' Fortify a tbl_graph for ggplot2 plotting
+#'
+#' In general `tbl_graph` objects are intended to be plotted by network
+#' visualisation libraries such as `ggraph`. However, if you do wish to plot
+#' either the node or edge data directly with `ggplot2` you can simply add the
+#' `tbl_graph` object as either the global or layer data and the currently
+#' active data is passed on as a regular data frame.
+#'
+#' @export
+#' @keywords internal
+#'
+fortify.tbl_graph <- function(model, data, ...) {
+  as_tibble(model)
+}
