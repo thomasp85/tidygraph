149577e0cfafa9e806a8ccd2b1cc8b0f3083ce5f
Thomas Lin Pedersen <thomasp85@gmail.com>
2018-11-20 13:10:33 +0100

    Fix #60 Add edge length if present in phylo

--- R/phylo.R
+++ R/phylo.R
@@ -1,12 +1,16 @@
 #' @describeIn tbl_graph Method for handling phylo objects from the ape package
+#' @importFrom igraph set_edge_attr
 #' @export
 as_tbl_graph.phylo <- function(x, directed = NULL, ...) {
   if (!requireNamespace("ape", quietly = TRUE)) {
     stop('The "ape" package is needed for this functionality to work', call. = FALSE)
   }
   if (is.null(directed)) directed <- ape::is.rooted(x)
-  as_tbl_graph(ape::as.igraph.phylo(x, directed = directed, ...))
+  gr <- ape::as.igraph.phylo(x, directed = directed, ...)
+  if (!is.null(x$edge.length)) gr <- set_edge_attr(gr, 'length', value = x$edge.length)
+  as_tbl_graph(gr)
 }
+
 #' @describeIn tbl_graph Method for handling evonet objects from the ape package
 #' @export
 as_tbl_graph.evonet <- function(x, directed = TRUE, ...) {
