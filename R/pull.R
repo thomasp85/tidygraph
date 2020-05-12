430ecfa1f3677974f179fe06482c183d8e1b5d3f
Thomas Lin Pedersen <thomasp85@gmail.com>
2020-05-12 08:14:10 +0200

    New approach to pull() that works with both dplyr versions

--- R/pull.R
+++ R/pull.R
@@ -1,17 +1,17 @@
+# TODO: Update signature to include `name` once we begin to depend on dplyr 1.0
+
 #' @export
 #' @importFrom dplyr pull
-pull.tbl_graph <- function(.data, var = -1, name = NULL) {
+pull.tbl_graph <- function(.data, var = -1, ...) {
   d_tmp <- as_tibble(.data)
   var <- enquo(var)
-  name <- enquo(name)
-  pull(d_tmp, !! var, !! name)
+  pull(d_tmp, !! var, ...)
 }
 #' @export
 #' @importFrom dplyr pull
-pull.morphed_tbl_graph <- function(.data, var, name = NULL) {
+pull.morphed_tbl_graph <- function(.data, var = -1, ...) {
   var <- enquo(var)
-  name <- enquo(var)
-  lapply(.data, pull, !! var, !! name)
+  lapply(.data, pull, !! var, ...)
 }
 #' @export
 dplyr::pull
